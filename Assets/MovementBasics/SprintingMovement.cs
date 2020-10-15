7fefa79a9175c181931fb1e7bcc4963f9cca8cd0
deztr0yer3927 <72624025+deztr0yer3927@users.noreply.github.com>
2020-10-15 12:20:57 -0500

    test1

---
+++ Assets/MovementBasics/SprintingMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SprintingMovement : MonoBehaviour
+{
+    // you can initialize variables at their declaration
+    public float walkSpeed = 4f;
+    public float sprintSpeed = 8f;
+
+    private float realSpeed;
+
+    void Update()
+    {
+        // you can use the ternary operator in this case
+        realSpeed = Input.GetKey(KeyCode.LeftShift) ? walkSpeed : sprintSpeed;
+    }
+
+    // physics (which movement is) should go into FixedUpdate
+    void FixedUpdate()
+    {
+        transform.Translate(realSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, realSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+    }
+
+}
---
+++ Assets/colision.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colision : MonoBehaviour
+{
+         if(Input.GetMouseButtonDown(0))
+         {
+             Vector2 mousePos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+    RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero, 100f);
+             if(hit.collider != null)
+             {
+                 Destroy(hit.collider);
+}
+         }
+
+    }
+
+   
+
+
+    
+}
+
+ 
