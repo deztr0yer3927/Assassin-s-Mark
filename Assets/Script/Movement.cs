2d25c9d674497799e6cdd5ce16af859e4c2c466f
deztr0yer3927 <72624025+deztr0yer3927@users.noreply.github.com>
2020-11-05 12:31:16 -0600

    Movement fix

--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -5,42 +5,46 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // horizontal rotation speed
+    public float horizontalSpeed = 1f;
+    // vertical rotation speed
+    public float verticalSpeed = 1f;
+    private float xRotation = 0.0f;
+    private float yRotation = 0.0f;
+    private Camera cam;
+
     void Start()
     {
-<<<<<<< Updated upstream
         cam = Camera.main;
+        
         Cursor.visible = false;
 
         Screen.lockCursor = true;
-=======
-        
->>>>>>> Stashed changes
+
     }
 
     // Update is called once per frame
     void Update()
     {
-<<<<<<< Updated upstream
-
+    
+        
+        float mouseX = Input.GetAxis("Mouse X") * horizontalSpeed;
+        float mouseY = Input.GetAxis("Mouse Y") * verticalSpeed;
+ 
+        yRotation += mouseX;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+ 
+        cam.transform.eulerAngles = new Vector3(xRotation, yRotation, 0.0f);
+        
         if (Input.GetKey(KeyCode.E))
         {
             Cursor.visible = true;
             Screen.lockCursor = false;
         }
 
+    
 
-
-        float mouseX = Input.GetAxis("Mouse X") * horizontalSpeed;
-        float mouseY = Input.GetAxis("Mouse Y") * verticalSpeed;
-
-        yRotation += mouseX;
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90, 90);
-
-        cam.transform.eulerAngles = new Vector3(xRotation, yRotation, 0.0f);
-=======
-        
->>>>>>> Stashed changes
+  
     }
 }
